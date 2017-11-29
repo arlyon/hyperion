03602313ecf63e317b2739950b88dca5adbb3822
arlyon <arlyon@me.com>
2017-11-29 14:39:09 +0000

    add heroku launch and CORS

--- back/__init__.py
+++ back/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask('project', template_folder='back/templates', static_folder='back/static')
+CORS(app)
 app.config['SECRET_KEY'] = 'secret'
 app.debug = True
 
