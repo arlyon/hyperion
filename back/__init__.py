aaedf06cf7d31c19c77d3b092e4ee2d47e42396a
arlyon <arlyon@me.com>
2017-12-15 14:15:04 +0000

    some logging to figure out whats happening

--- back/__init__.py
+++ back/__init__.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_sslify import SSLify
 import os
+import sys
 
 app = Flask('project', template_folder='back/templates', static_folder='back/static')
 
 # if running on heroku
 if 'DYNO' in os.environ:
+    print("Running App in Production Mode")
+    sys.stdout.flush()
     SSLify(app)
     app.debug = False
     app.config = os.environ.get('FLASK_KEY')
 else:
+    print("Running App in Debug Mode")
+    sys.stdout.flush()
     app.debug = True
     app.config['SECRET_KEY'] = 'dev'
 
